97fa7f0484b57127774c87641e2b1ee63c516353
Sumit Bhalothia <sumitbhalothia103@gmail.com>
2026-08-24 16:26:26 +0530

    Update rag_pipeline.py

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -9,10 +9,6 @@ EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 
 LLM_MODEL = "microsoft/Phi-3-mini-4k-instruct"
 
-# Reliable Serverless Chat Model
-#LLM_MODEL = "meta-llama/Llama-3.2-3B-Instruct"
-
-
 # =========================
 # Hugging Face Client
 # =========================
