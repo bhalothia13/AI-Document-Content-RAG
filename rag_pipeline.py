27d65541620d56ea494711f5f8fd7c74a074a79f
Sumit Bhalothia <sumitbhalothia103@gmail.com>
2026-08-24 15:55:56 +0530

    Update rag_pipeline.py

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -5,8 +5,12 @@ from huggingface_hub import InferenceClient
 # =========================
 # Hugging Face Models
 # =========================
-
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+
+# Pure serverless API ke liye working model:
+#LLM_MODEL = "Qwen/Qwen2.5-72B-Instruct"
+# OR alternative option:
+# LLM_MODEL = "mistralai/Mistral-7B-Instruct-v0.2"
 
 # Reliable Serverless Chat Model
 LLM_MODEL = "meta-llama/Llama-3.2-3B-Instruct"
